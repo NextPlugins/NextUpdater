fec4b728157544b6367dfc61d90b8b57f65f88fd
eikefab <fabricio.eike.s@gmail.com>
2021-03-22 16:31:58 -0300

    feat(core): registering by default NextPlugins' plugins

--- src/main/java/com/nextplugins/nextupdater/NextUpdater.java
+++ src/main/java/com/nextplugins/nextupdater/NextUpdater.java
@@ -3,6 +3,7 @@ package com.nextplugins.nextupdater;
 import com.nextplugins.nextupdater.api.PluginUpdater;
 import me.bristermitten.pdm.PluginDependencyManager;
 import org.bstats.bukkit.Metrics;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NextUpdater extends JavaPlugin {
@@ -13,9 +14,19 @@ public final class NextUpdater extends JavaPlugin {
             PluginUpdater updater = new PluginUpdater(this, "NextUpdater");
 
             updater.init();
+            registerDefaultPlugins();
 
             new Metrics(this, 10773);
         });
+    }
+
+    private void registerDefaultPlugins() {
+        for (Plugin plugin : getServer().getPluginManager().getPlugins()) {
+            if (!plugin.getName().startsWith("Next")) continue;
+
+            PluginUpdater updater = new PluginUpdater(plugin, plugin.getName());
+            updater.init();
+        }
     }
 
 }
