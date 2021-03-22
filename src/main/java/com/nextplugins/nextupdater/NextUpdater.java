b79b24cda1542063c9063e31d3c5343d1983e5d2
eikefab <fabricio.eike.s@gmail.com>
2021-03-22 16:07:58 -0300

    impl(metrics): implementing metrics

--- src/main/java/com/nextplugins/nextupdater/NextUpdater.java
+++ src/main/java/com/nextplugins/nextupdater/NextUpdater.java
@@ -2,6 +2,7 @@ package com.nextplugins.nextupdater;
 
 import com.nextplugins.nextupdater.api.PluginUpdater;
 import me.bristermitten.pdm.PluginDependencyManager;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NextUpdater extends JavaPlugin {
@@ -10,7 +11,10 @@ public final class NextUpdater extends JavaPlugin {
     public void onEnable() {
         PluginDependencyManager.of(this).loadAllDependencies().thenRun(() -> {
             PluginUpdater updater = new PluginUpdater(this, "NextUpdater");
+
             updater.init();
+
+            new Metrics(this, 10773);
         });
     }
 
